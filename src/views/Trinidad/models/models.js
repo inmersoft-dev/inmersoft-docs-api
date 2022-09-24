9a981d0d46dae57ed3c204d3efca6a64f03f2683
Sito8943 <sito8943@gmail.com>
2022-09-24 18:10:14 -0400

    doing route model

--- src/views/Trinidad/models/models.js
+++ src/views/Trinidad/models/models.js
@@ -28,12 +28,6 @@ const models = {
               "boolean[3]: [dónde comer, dónde dormir, experiencia]",
           },
         ],
-        noPlaceTypes: [
-          {
-            id: "number: placeType unique id",
-            name: "string: placeType name",
-          },
-        ],
         routes: [
           {
             date: "number: date of creation's milliseconds",
@@ -87,7 +81,6 @@ const models = {
             ],
           },
         ],
-        noRoutes: [{ id: "number: unique id", name: "string: route name" }],
         phone: "string: telephone number",
         address: "string: address",
         services: "string[]: services name",
@@ -96,6 +89,136 @@ const models = {
         video: "string: web video",
         latitude: "number",
         longitude: "number",
+        content: [
+          {
+            value: {
+              content: "string: HTML content",
+            },
+            type: "string: text",
+          },
+          {
+            value: {
+              url: "string: video url",
+            },
+            type: "string: video",
+          },
+          {
+            value: {
+              id: "string: imageKit unique file id",
+              blurHash: "string: blurHash",
+              url: "string: image source url",
+            },
+            type: "string: photo",
+          },
+          {
+            value: {
+              title: "string: accordion title",
+              content: "string: accordion content",
+            },
+            type: "string: accordion",
+          },
+        ],
+        comments: [
+          {
+            comment: "string: customer comment content",
+            stars: "number: stars",
+          },
+        ],
+      },
+    ],
+  },
+  route: {
+    status: "number: 200 - Ok / 422 - not found",
+    data: [
+      {
+        date: "number: date of creation's milliseconds",
+        id: "number: unique id",
+        name: "string: name",
+        description: "string: short description",
+        headerImages: [
+          {
+            id: "string: imageKit unique file id",
+            blurHash: "string: blurHash",
+            url: "string: image source url",
+          },
+        ],
+        panoramic: {
+          id: "string: imageKit unique file id",
+          blurHash: "string: blurHash",
+          url: "string: image source url",
+        },
+        places: [
+          {
+            date: "number: date of creation's milliseconds",
+            id: "number: unique id",
+            name: "string: name",
+            description: "string: short description",
+            headerImages: [
+              {
+                id: "string: imageKit unique file id",
+                blurHash: "string: blurHash",
+                url: "string: image source url",
+              },
+            ],
+            panoramic: {
+              id: "string: imageKit unique file id",
+              blurHash: "string: blurHash",
+              url: "string: image source url",
+            },
+            placeType: [
+              {
+                id: "number: placeType unique id",
+                name: "string: placeType name",
+              },
+            ],
+            phone: "string: telephone number",
+            address: "string: address",
+            services: "string[]: services name",
+            socialMedia: "string[]: social media URLs",
+            web: "string: web site",
+            video: "string: web video",
+            latitude: "number",
+            longitude: "number",
+            content: [
+              {
+                value: {
+                  content: "string: HTML content",
+                },
+                type: "string: text",
+              },
+              {
+                value: {
+                  url: "string: video url",
+                },
+                type: "string: video",
+              },
+              {
+                value: {
+                  id: "string: imageKit unique file id",
+                  blurHash: "string: blurHash",
+                  url: "string: image source url",
+                },
+                type: "string: photo",
+              },
+              {
+                value: {
+                  title: "string: accordion title",
+                  content: "string: accordion content",
+                },
+                type: "string: accordion",
+              },
+            ],
+            comments: [
+              {
+                comment: "string: customer comment content",
+                stars: "number: stars",
+              },
+            ],
+          },
+        ],
+        activities: "string[]: activities name",
+        web: "string: web site",
+        video: "string: web video",
         content: [
           {
             value: {
